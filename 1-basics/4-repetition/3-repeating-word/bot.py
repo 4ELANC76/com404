5495f399e73b5b471f8b40e8a385f0c4eeb159dd
4ELANC76 <43994813+4ELANC76@users.noreply.github.com>
2018-10-09 16:31:27 +0100

    Create 3-repeating-word/bot.py

---
+++ 1-basics/4-repetition/3-repeating-word/bot.py
@@ -0,0 +1,10 @@
+#ask the user how many robots they want to display
+print("Enter a word?")
+word = input()
+
+#work out length of word
+length = len(word)
+
+#display words
+for count in range (0,length,1):
+  print(word)
