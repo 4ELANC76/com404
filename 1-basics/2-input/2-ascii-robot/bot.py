a3487ae78bf22069db58fc9f2e136342ce92125c
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-12 19:02:02 +0100

    Create bot.py

---
+++ 1-basics/2-input/2-ascii-robot/bot.py
@@ -0,0 +1,9 @@
+print("Please enter a character for the eye")
+o = input()
+print("Beep's expression is now as follows:")
+
+print("##########")
+print("#        #")
+print("#  0", o, "\t" "#")
+print("# ------ #")
+print("##########")
