d3409ba1e93a5c1cfde19d391989a5917c4d0d7f
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-12 21:17:01 +0100

    Create bot.py

---
+++ 1-basics/3-decision/2-if-else/bot.py
@@ -0,0 +1,9 @@
+activity = input("Please enter the activity to be performed.")
+activity = str(activity)
+
+if (activity == "calculate"):
+  print("Performing calculations...")
+else:
+  print("Performing activity...")
+print()
+print("Activity completed!")
