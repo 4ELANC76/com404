207aca7e99ccdbfb0f427da0e193bab2e3274e8c
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-12 20:54:22 +0100

    Create bot.py

---
+++ 1-basics/3-decision/0.5-if/bot.py
@@ -0,0 +1,13 @@
+#check whether first number is bigger than the second number.
+#Assum first_number and second_number are existing variables.
+
+first_number = input("Please enter your first number.")
+first_number = int(first_number)
+second_number = input("Please insert your second number")
+second_number = int(second_number)
+
+if (first_number > second_number):
+  print("First is bigger!")
+if (first_number < second_number):
+  print("Second is bigger!")
+print("Done!")
