ed66af2c5b77a81fd957e9b856a4318aadbf68f2
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-12 21:06:33 +0100

    Update bot.py
    added alternative responses to the user answer

--- 1-basics/3-decision/1-if/bot.py
+++ 1-basics/3-decision/1-if/bot.py
@@ -3,4 +3,8 @@ book = str(book)
 
 if (book == "adventure"):
   print("I like adventure books too!")
+if (book == "fiction"):
+  print("Wow! A fiction book!")
+if (book == "fantasy"):
+  print("My favourite fantasy book is Harry Potter")
 print("Finshed reading the book.")
