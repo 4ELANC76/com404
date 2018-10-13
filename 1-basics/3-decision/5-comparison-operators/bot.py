9a782dc4a6e6d6178b6d3cbabd1cc36ae36c7175
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-13 17:24:17 +0100

    Create bot.py

---
+++ 1-basics/3-decision/5-comparison-operators/bot.py
@@ -0,0 +1,11 @@
+first_number = input("Please enter your first number.")
+print(first_number)
+second_number = input("Please enter your second number.")
+print(second_number)
+
+if (first_number > second_number):
+  print("The second number is smallest.")
+elif (first_number < second_number):
+  print("The first number is smallest.")
+else:
+  print("Both are equal!")
