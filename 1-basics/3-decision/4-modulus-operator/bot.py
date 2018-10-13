4410f83035770cca6513798ffdc5b8758b306543
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-13 16:17:19 +0100

    Create bot.py

---
+++ 1-basics/3-decision/4-modulus-operator/bot.py
@@ -0,0 +1,7 @@
+number = input("Please enter a whole number")
+number = int(number)
+
+if (number % 2==0):
+  print("The number", number, "is an even number.")
+else:
+  print("The number", number, "is an odd number.")
