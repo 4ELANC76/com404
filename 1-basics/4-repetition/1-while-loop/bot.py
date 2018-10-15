48a72baee0753a7728c601d3f148f56167369bf4
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-15 20:22:40 +0100

    Update bot.py

--- 1-basics/4-repetition/1-while-loop/bot.py
+++ 1-basics/4-repetition/1-while-loop/bot.py
@@ -1,14 +1,17 @@
 robots = input("How many ascii robots should I draw?")
 robots = int(robots)
+count = 0
+
 
 print("Here I go:")
 print()
-for count in range (0, robots, 1):
+while(count < robots):
   print("#########")
   print("#       #")
   print("# 0   0 #")
   print("|   v   |")
   print("|  ---  |")
   print("|_______|")
+  count = count + 1
 print()
 print("DONE!")
