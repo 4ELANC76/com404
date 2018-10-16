8078ee9f89c3d358dd482879fab1c9cc6b67c948
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-16 15:07:47 +0100

    Create 5-functions/1-greeting/bot.py

---
+++ 1-basics/5-functions/1-greeting/bot.py
@@ -0,0 +1,10 @@
+#create the user defined function
+def greet_user():
+  print("Please enter your name.")
+  name = str(input())
+  print("Hello",name)
+
+#call the function 3 times - the whole thing 3 times
+greet_user()
+greet_user()
+greet_user()
