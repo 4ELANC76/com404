a6aac4c7ccd01087d32db2bb6ff4ddb58be7e987
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-16 15:55:39 +0100

    Create 4-simba/bot.py

---
+++ 1-basics/5-functions/4-simba/bot.py
@@ -0,0 +1,16 @@
+def roar(num_roars):
+  for count in range(0,num_roars,1):
+    print("roar!")
+
+
+  if (num_roars >= 3):
+    print("ROAR!")
+  else:
+    print("cough! cough!")
+    
+    
+    #Write your code here
+
+#The following are calls to the function for testing purposes
+roar(1)
+roar(3)
