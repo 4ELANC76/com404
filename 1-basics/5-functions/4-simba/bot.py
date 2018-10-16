ebea57a5324b10f10c0c17dc130608b0d1c5e665
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-16 15:58:31 +0100

    Update bot.py

--- 1-basics/5-functions/4-simba/bot.py
+++ 1-basics/5-functions/4-simba/bot.py
@@ -1,13 +1,15 @@
 def roar(num_roars):
+  print("Simba says")
+  #loop
   for count in range(0,num_roars,1):
     print("roar!")
 
-
+#decision
   if (num_roars >= 3):
     print("ROAR!")
   else:
     print("cough! cough!")
-    
+  
     
     #Write your code here
 
