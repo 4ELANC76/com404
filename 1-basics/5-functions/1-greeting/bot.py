2ae99da461310f69ed3975a8c0d22cbca9023460
C E <43994813+4ELANC76@users.noreply.github.com>
2018-10-22 15:07:10 +0100

    Update bot.py

--- 1-basics/5-functions/1-greeting/bot.py
+++ 1-basics/5-functions/1-greeting/bot.py
@@ -1,8 +1,16 @@
+#create the display box
+def display_box(name):
+  print("*" * (len(name) + 10))
+  print("* Hello", name, "*")
+  print("*" * (len(name) + 10))
+ 
 #create the user defined function
 def greet_user():
-  print("Please enter your name.")
+  print("Please enter your name")
   name = str(input())
-  print("Hello",name)
+  display_box(name)
+  
+greet_user()
 
 #call the function 3 times - the whole thing 3 times
 greet_user()
